2e535d2a2846a88fdd6aa45f078a3ae652e72c2c
vanillacoke <fariha.tasheem@hotmail.com>
2016-09-27 12:41:25 +1300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
- 
-
 /* Code for Assignment ?? 
  * Name:
  * Usercode:
  * ID:
  */
 
-
 import ecs100.*;
 import java.util.*;
 import java.io.*;
 import java.awt.*;
-
 
 /** <description of class Main>
  */
@@ -22,10 +18,10 @@ public class Main{
     private ToolPath tool_path;
     // state of the GUI
     private int state; // 0 - nothing
-                       // 1 - inverse point kinematics - point
-                       // 2 - enter path. Each click adds point  
-                       // 3 - enter path pause. Click does not add the point to the path
-    
+    // 1 - inverse point kinematics - point
+    // 2 - enter path. Each click adds point  
+    // 3 - enter path pause. Click does not add the point to the path
+
     /**      */
     public Main(){
         UI.initialise();
@@ -35,20 +31,22 @@ public class Main{
         UI.addButton("Load path XY", this::load_xy);
         UI.addButton("Save path Ang", this::save_ang);
         UI.addButton("Load path Ang:Play", this::load_ang);
-                
-       // UI.addButton("Quit", UI::quit);
+        UI.addButton("CIRCLE", this::doCircle);
+        UI.addButton("Quit", UI::quit);
         UI.setMouseMotionListener(this::doMouse);
         UI.setKeyListener(this::doKeys);
 
-
         //ServerSocket serverSocket = new ServerSocket(22); 
-        
         this.arm = new Arm();
         this.tool_path = new ToolPath();
         this.drawing = new Drawing();
         this.run();
         arm.draw();
     }
+    public void doCircle(){
+        state = 4;
+    }
+
     
     public void doKeys(String action){
         UI.printf("Key :%s \n", action);
@@ -56,65 +54,62 @@ public class Main{
             // break - stop entering the lines
             state = 3;
             //
-          
+
         }
-               
+
     }
-    
-    
+
     public void doMouse(String action, double x, double y) {
-         //UI.printf("Mouse Click:%s, state:%d  x:%3.1f  y:%3.1f\n",
-         //   action,state,x,y);
+        //UI.printf("Mouse Click:%s, state:%d  x:%3.1f  y:%3.1f\n",
+        //   action,state,x,y);
         UI.clearGraphics();
         String out_str=String.format("%3.1f %3.1f",x,y);
         UI.drawString(out_str, x+10,y+10);
-         // 
-         if ((state == 1)&&(action.equals("clicked"))){
-          // draw as 
-          
-          arm.inverseKinematic(x,y);
-          arm.draw();
-          return;
-        }
-        
-         if ( ((state == 2)||(state == 3))&&action.equals("moved") ){
-          // draw arm and path
-          arm.inverseKinematic(x,y);
-          arm.draw();
-         
-          // draw segment from last entered point to current mouse position
-          if ((state == 2)&&(drawing.get_path_size()>0)){
-            PointXY lp = new PointXY();
-            lp = drawing.get_path_last_point();
-            //if (lp.get_pen()){
-               UI.setColor(Color.GRAY);
-               UI.drawLine(lp.get_x(),lp.get_y(),x,y);
-           // }
-          }
-           drawing.draw();
-        }
-        
-        // add point
-        if(arm.inverseKinematic(x,y) == true){
-        if (   (state == 2) &&(action.equals("clicked"))){
-            // add point(pen down) and draw
-            UI.printf("Adding point x=%f y=%f\n",x,y);
-            drawing.add_point_to_path(x,y,true); // add point with pen down
-            
-            
+        // 
+        if ((state == 1)&&(action.equals("clicked"))){
+            // draw as 
+
             arm.inverseKinematic(x,y);
             arm.draw();
-            drawing.draw();
-            drawing.print_path();
+            return;
         }
-    }
-        
-        
-        if (   (state == 3) &&(action.equals("clicked"))){
+
+        if ( ((state == 2)||(state == 3))&&action.equals("moved") ){
+            // draw arm and path
+            arm.inverseKinematic(x,y);
+            arm.draw();
+
+            // draw segment from last entered point to current mouse position
+            if ((state == 2)&&(drawing.get_path_size()>0)){
+                PointXY lp = new PointXY();
+                lp = drawing.get_path_last_point();
+                //if (lp.get_pen()){
+                UI.setColor(Color.GRAY);
+                UI.drawLine(lp.get_x(),lp.get_y(),x,y);
+                // }
+            }
+            drawing.draw();
+        }
+
+        // add point
+        if(arm.inverseKinematic(x,y) == true){
+            if (   (state == 2) &&(action.equals("clicked"))){
+                // add point(pen down) and draw
+                UI.printf("Adding point x=%f y=%f\n",x,y);
+                drawing.add_point_to_path(x,y,true); // add point with pen down
+
+                arm.inverseKinematic(x,y);
+                arm.draw();
+                drawing.draw();
+                drawing.print_path();
+            }
+        }
+
+        if ((state == 3) &&(action.equals("clicked"))){
             // add point and draw
             //UI.printf("Adding point x=%f y=%f\n",x,y);
             drawing.add_point_to_path(x,y,false); // add point wit pen up
-            
+
             arm.inverseKinematic(x,y);
             arm.draw();
             drawing.draw();
@@ -122,25 +117,45 @@ public class Main{
             state = 2;
         }
         
-        
+       if ((state == 4) &&(action.equals("clicked"))){
+            double x1 = x ;
+            double y1 = y ;
+            if(arm.inverseKinematic(x1,y1) == true){
+            for(int i = 0; i<=360; i=i+5){
+                
+                x1 = x + (100/2)*Math.cos(i*Math.PI/180);
+                y1 = y + (100/2)*Math.sin(i*Math.PI/180);
+                
+                drawing.add_point_to_path(x1,y1,true);
+                arm.inverseKinematic(x1,y1);
+                //arm.draw();
+                drawing.draw();
+                drawing.print_path();
+            }
+        }
+            
+            
+       
+            
+        }
+
     }
-   
     
     public void save_xy(){
         state = 0;
         String fname = UIFileChooser.save();
         drawing.save_path(fname);
     }
-    
+
     public void enter_path_xy(){
-         state = 2;
+        state = 2;
     }
-    
+
     public void inverse(){
-         state = 1;
-         arm.draw();
+        state = 1;
+        arm.draw();
     }
-    
+
     public void load_xy(){
         state = 0;
         String fname = UIFileChooser.open();
@@ -148,7 +163,7 @@ public class Main{
         drawing.draw();
         arm.draw();
     }
-    
+
     // save angles into the file
     public void save_ang(){
         state = 0; 
@@ -156,16 +171,13 @@ public class Main{
         tool_path.convert_drawing_to_angles(drawing,arm,fname);
         tool_path.convert_angles_to_pwm(arm);
         tool_path.save_pwm_file(fname);
-        
+
         //sudo./arm
-        
+
     }
-    
-    
+
     public void load_ang(){
-        
     }
-    
     public void run() {
         while(true) {
             arm.draw();
